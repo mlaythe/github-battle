e8eb3667d20f7350ced9f43d337862eea7ccd69c
Michael Laythe <mrlaythe24@aol.com>
2016-04-07 21:18:00 -0700

    adds a parent component called Avatar that renders the image, name, & link to github acct to the DOM... also added run script to package.json to start up webpack dev server

--- app/index.js
+++ app/index.js
@@ -1,16 +1,52 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+  name: 'Michael Laythe',
+  username: 'mlaythe',
+  image: 'https://avatars.githubusercontent.com/mlaythe'
+}
 
-  render: function() {
+var ProfilePic = React.createClass({
+  render: function(){
+    return (
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
     return (
       <div>
-        Hello World
+        {this.props.name}
       </div>
     );
   }
-
 });
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
