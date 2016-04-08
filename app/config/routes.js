72ec5472f10fa9c9f2abb90a3afe891143217831
Michael Laythe <mrlaythe24@aol.com>
2016-04-07 22:15:46 -0700

    added new routes & basic UI for player prompt screen

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,11 +6,14 @@ var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' component={PromptContainer}/>
+      <Route path='playerTwo/:playerOne' componenet={PromptContainer} />
     </Route>
   </Router>
 );
