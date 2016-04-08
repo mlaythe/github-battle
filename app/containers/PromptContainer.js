a16b209bd30ce1a758a0ca55031da6634d053f7c
Michael Laythe <mrlaythe24@aol.com>
2016-04-07 22:54:23 -0700

    added logic in order to be able to route to different paths & update the path url according to the username entered

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,14 +1,49 @@
 var React = require('react');
+var transparentBg = require('../styles/').transparentBg;
 
 var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      username: ''
+    }
+  },
+  onSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      });
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+  onUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
   render: function(){
     return (
-      <div className='jumbotron col-sm-6 col-sm-offset-3 text-center'>
-        <h1>Some header text</h1>
+      <div className='jumbotron col-sm-6 col-sm-offset-3 text-center' style={transparentBg}>
+        <h1>{this.props.route.header}</h1>
         <div className='col-sm-12'>
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className='form-group'>
               <input className='form-control' placeholder='github username'
+                onChange={this.onUpdateUser}
+                value={this.state.username}
                 type='text' />
             </div>
             <div className='form-group col-sm-4 col-sm-offset-4'>
