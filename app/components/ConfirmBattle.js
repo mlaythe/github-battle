a6279b838185f619bde79bca2094664594a4c51c
Michael Laythe <mrlaythe24@aol.com>
2016-04-08 16:54:37 -0700

    refactored out some UI into MainContainer.js

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,15 +4,13 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-var puke = object => {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-};
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 var ConfirmBattle = props => {
   return props.isLoading === true
-    ? <p> Loading </p>
-  : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+  ? <Loading />
+  : <MainContainer>
       <h1>Confirm Players</h1>
       <div className='col-sm-8 col-sm-offset-2'>
         <UserDetailsWrapper header='Player 1'>
@@ -37,7 +35,7 @@ var ConfirmBattle = props => {
           </Link>
         </div>
       </div>
-    </div>
+    </MainContainer>
 };
 
 ConfirmBattle.propTypes = {
